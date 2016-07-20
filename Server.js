c7892685c9aa6dd98e5ffba1fde6c52e506ce327
Joel ROTELLI <support@skr-creation.com>
2016-07-20 22:21:20 +0200

    Remove next garbage

--- Server.js
+++ Server.js
@@ -11,17 +11,14 @@ var app = express();
 var router = express.Router();
 var remind = new Reminder();
 var whichGarbage = planning.whichGarbage();
-var nextGarbage = planning.nextGarbage();
 
 console.log('A sortir demain : ' + whichGarbage.name);
-console.log('Next : ' + nextGarbage.day);
 
 var pusher = new push({
     user: config.pushover.user,
     token: config.pushover.token
 });
 
-console.log(config);
 
 router.use(function (req, res, next) {
     //console.log("/" + req.method);
@@ -32,26 +29,18 @@ router.get("/", function (req, res) {
     res.render('index.twig', {
         name: whichGarbage.name,
         image: whichGarbage.image,
-        color: whichGarbage.color,
-        next: nextGarbage
+        color: whichGarbage.color
     });
 });
 
 
-app.use("/", router);
-app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 5000, function () {
-    console.log("Server HTTP démarré");
-});
-
-
-remind.at('20:00', function () {
+remind.at('22:24', function () {
 
     //Push alert only if we have a color)
     if (whichGarbage.color != '') {
         var msg = {
-            message: whichGarbage.name,   // required
+            message: "Pensez au bac " + whichGarbage.name + " pour demain !",   // required
             title: "Hey",
             sound: 'bike',
             device: 'lg-d331',
@@ -67,3 +56,12 @@ remind.at('20:00', function () {
 
     }
 });
+
+
+app.use("/", router);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 5000, function () {
+    console.log("Server HTTP démarré");
+});
+
